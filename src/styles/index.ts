1d14c71d521d2fcf6323cc483a400bb89c43c727
cieliocas <francielioevangelista@hotmail.com>
2025-02-02 11:09:12 -0300

    feat: implement sidebar and task list components; update App layout with global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 export const EstiloGlobal = createGlobalStyle`
   body {
@@ -7,4 +7,9 @@ export const EstiloGlobal = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
   }
+`
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 224px auto;
 `
